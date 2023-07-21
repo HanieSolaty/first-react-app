fbc42ccf4ebebf27e1f55bfb6f7434f989fe74e9
HanieSolaty <88339589+HanieSolaty@users.noreply.github.com>
2023-07-21 18:09:26 +0330

    remove react-loading component

--- src/Loader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactLoading from "react-loading";
-
-const Loader = ({ type, color }) => (
-  <ReactLoading type={type} color={color} height={"5%"} width={"5%"} />
-);
-
-export default Loader;
